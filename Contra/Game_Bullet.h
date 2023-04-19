212ce57bdeb43458a326001ff13d5af06e49f2a5
HuynhDangQuang <quangptnk@gmail.com>
2023-04-19 21:51:30 +0700

    Add class Game_Bullet and Rifle bullet.

---
+++ Contra/Game_Bullet.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "Game_MovableObject.h"
+
+#define B_OWNER_PLAYER 0
+#define B_OWNER_ENEMY 1
+
+class Game_Bullet : public Game_MovableObject
+{
+	protected:
+		// Used by other objects.
+		// Base on OwnerID, objects will ignore this bullet or not.
+		int _OwnerID = 0;
+		int _Damage = 1;
+	public:
+		Game_Bullet(float x, float y, int z, int width, int height) : Game_MovableObject(x, y, z, width, height)
+		{
+			_needScanCollision = false;
+			_moveSpd = 0.5;
+		}
+
+		// Used by other objects.
+		// Base on OwnerID, objects will ignore this bullet or not.
+		int OwnerID() { return _OwnerID; }
+		int Damage() { return _Damage; }
+
+		// Can this object ingore blocking object and move through it?
+		// If YES, collision framework will skip this collision event.
+		bool IgnoreBlocking() override { return 1; }
+
+		void Update(DWORD dt) override;
+		void SetAngle(float degree);
+		void SetTargetPos(float x, float y);
+};
+
