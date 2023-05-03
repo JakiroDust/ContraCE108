fe442c1c7e84efc652c80b69742634665099f0ef
HuynhDangQuang <quangptnk@gmail.com>
2023-05-03 22:57:15 +0700

    Temporarily remove sniper (unusable now).

--- Contra/Scene_Battle.cpp
+++ Contra/Scene_Battle.cpp
@@ -207,7 +207,9 @@ void Scene_Battle::Create_Stage_Demo()
     SS->playBGM(BGM_JUNGLE);
     unique_ptr<Obj_ContraBot> bot (new Obj_ContraBot(80, 40, 2)); add_object(move(bot));
     unique_ptr<Obj_SmartBot>smartbot(new Obj_SmartBot(100, 40, 2)); add_object(move(smartbot));
-    unique_ptr<Enemy_Sniper> sniper(new Enemy_Sniper(300, 200, 2)); add_object(move(sniper));
+    
+    // Can't use now. Wait for adding sprites.
+    //unique_ptr<Enemy_Sniper> sniper(new Enemy_Sniper(300, 200, 2)); add_object(move(sniper));
 
     unique_ptr<Game_Blocker> block1 ( new Game_Blocker(-18, 0, 1, 20, GAMESCREEN_HEIGHT - 20));
     unique_ptr<Game_Blocker> block2 ( new Game_Blocker(_mapWidth - 20, 1, 0, 20, GAMESCREEN_HEIGHT - 20));
