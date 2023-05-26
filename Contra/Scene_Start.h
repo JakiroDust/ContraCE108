71b859c96a62ea6fb3def4d8bce119e3636e971c
HuynhDangQuang <quangptnk@gmail.com>
2023-05-26 10:40:35 +0700

    Fixed Scene_Start

--- Contra/Scene_Start.h
+++ Contra/Scene_Start.h
@@ -1,20 +1,18 @@
 #pragma once
 #include "Scene_Base.h"
-class Scene_Start :
-    public Scene_Base
+class Scene_Start : public Scene_Base
 {
-    void KeyDownEventHandler(int KeyCode) override;
     void init();
     bool startTimer = false;
-    DWORD _waitingTick = 500;
+    DWORD _waitingTick = 500;  
+public:
+    Scene_Start(): Scene_Base() {};
+    void KeyDownEventHandler(int KeyCode) override;
     void Update(DWORD dt) override;
     void Render() override;
     void Load() override;
     void Unload() override;
     void nextScene();
-    
-public:
-    Scene_Start(): Scene_Base() {};
     ~Scene_Start() { Unload(); };
 };
 
